f7c142f9be77564c403fa992f59c4c26da5d77f7
MartiiPartii <125606563+MartiiPartii@users.noreply.github.com>
2025-09-13 10:28:48 +0300

    Fix Navigation

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,15 +1,40 @@
+import { verifyToken } from "@/lib/jwt/token"
 import { AppBar, Button, Stack, Toolbar, Typography } from "@mui/material"
+import { cookies } from "next/headers"
 import Link from "next/link"
 
-const Header = () => {
+const Header = async () => {
+    const cookieStore = await cookies()
+    const token = cookieStore.get("token")?.value
+    let isAuthenticated = false
+
+    try {
+        if(token) {
+            const user = await verifyToken(token, process.env.JWT_SECRET!)
+            if(user) isAuthenticated = true
+        }
+        else throw new Error()
+    } catch (err) {
+
+    }
+
+
     return (
         <AppBar>
                 <Toolbar sx={{ backgroundColor: "primary.contrastText" }}>
-                    <Typography variant="h4" color="primary">TeeStore</Typography>
+                    <Link href="/"><Typography variant="h4" color="primary">TeeStore</Typography></Link>
 
                     <Stack direction={"row"} gap={2} flex={1} justifyContent="end" alignItems={"end"}>
                         <Link href={"/browse"}><Button>Browse</Button></Link>
-                        <Link href={"/register"}><Button color="accent" variant="contained">Join now</Button></Link>
+                        
+                        {
+                            isAuthenticated ?
+                            <>
+                                <Link href={"/sell-tshirt"}><Button>Sell</Button></Link>
+                            </>
+                            :
+                            <Link href={"/register"}><Button color="accent" variant="contained">Join now</Button></Link>
+                        }
                     </Stack>
                 </Toolbar>
         </AppBar>
