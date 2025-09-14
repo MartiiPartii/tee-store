2d3b5d5a81f362b760fc74734ee463bd195629cd
MartiiPartii <125606563+MartiiPartii@users.noreply.github.com>
2025-09-14 03:21:30 +0300

    Middleware

--- middleware.ts
+++ middleware.ts
@@ -2,18 +2,23 @@ import { NextRequest, NextResponse } from "next/server";
 import { isAuth } from "./lib/authentication/authenticate";
 
 const protectedPostRoutes = ['/api/store/shirt']
+const protectedRoutes = ['/profile', '/my-shirts', '/orders', '/orders/[id]', '/sell-tshirt', '/shirt/[id]/purchase']
 
 export default async function middleware(req: NextRequest) {
     const path = req.nextUrl.pathname
-    const isProtected = (protectedPostRoutes.includes(path) && req.method === "POST")
+    const isProtectedApi = (protectedPostRoutes.includes(path) && req.method === "POST")
+    const isProtectedPage = protectedRoutes.includes(path)
 
     const isAuthenticated = await isAuth(req)
 
-    if(isProtected && !isAuthenticated) {
+    if(isProtectedApi && !isAuthenticated) {
         return NextResponse.json(
             { message: "You need to log in to perform this operation" },
             { status: 401 }
         )
+    }
+    if(isProtectedPage && !isAuthenticated) {
+        return NextResponse.redirect(new URL('/login', req.url))
     }
 
     return NextResponse.next()
